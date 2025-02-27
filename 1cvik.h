e70d0d6162dba649e3199b98debe998b7f1f3374
Yaroslav <gavadaasd@gmail.com>
2025-02-28 00:57:58 +0100

    Intersaction editing
    Intersaction() function works correctly now: finds the intersaction of
    two arrays and writes it to the third array without duplicates. The Size
    of third array depends on the amount of intersactions between 1st and 2nd
    arrays.

---
+++ 1cvik.h
@@ -0,0 +1,8 @@
+#ifndef CVIK1
+#define CVIK1
+#endif
+
+void SizeOfArrays(int *n, int *m);
+void FillArraysUp(int array1[], int array2[], int n, int m);
+void Intersection(int array1[], int array2[], int n, int m);
+void Union(int array1[], int array2[], int n);
