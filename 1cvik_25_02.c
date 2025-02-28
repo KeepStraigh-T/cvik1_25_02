86cc134f7bdf535273c5b0d193c60abe53fd6b67
Yaroslav <gavadaasd@gmail.com>
2025-02-28 01:13:13 +0100

    Intersection() function final version
    Intersection function doens't count any duplicates now.
    Add new function to get input from user (size of each array).
    Add new function to to fill the arrays with random numbers.
    
    Updated gitignore file to ignore .vscode folder.

--- 1cvik_25_02.c
+++ 1cvik_25_02.c
@@ -1,120 +1,161 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-void Intersection(int array1[], int array2[], int n);
-void Union(int array1[], int array2[], int n);
+#include "1cvik.h"
 
-int main() {
+int main()
+	{
+		srand(time(NULL));
+
+		int First_array_size, Second_array_size;
+
+		SizeOfArrays(&First_array_size, &Second_array_size);
+
+		int First_array[First_array_size], Second_array[Second_array_size];
+
+		FillArraysUp(First_array, Second_array, First_array_size, Second_array_size);
 		
-		int array1[] = {3, 3, 6, 3, 3};
-		int array2[] = {2, 3, 6, 9, 4};
-
-		int n = sizeof(array1) / sizeof(array1[0]);
-
-		Intersection(array1, array2, n);
-		// Union(array1, array2, n);
+		Intersection(First_array, Second_array, First_array_size, Second_array_size);
 
 		return 0;
-}
+	}
 
-
-void Intersection(int array1[], int array2[], int n)
-	{
-		int Amount_of_intersections = 0;
-		int flag_duplicate;
-
-		for(int i = 0; i < n; i++)
+	void SizeOfArrays(int *n, int *m)
 		{
-			for(int j = 0; j < n; j++)
+			int scanf_result;
+			int buffer_cleaner;
+			printf("Enter the size of the first array: ");
+			// scanf_result = scanf("%d", n);
+			if(scanf("%d", n) != 1 || *n <= 0)
+				{ 
+					while(buffer_cleaner = getchar() != '\n' && buffer_cleaner != EOF);
+					puts("Invalid input!");
+					exit(1);
+				}
+
+			while(buffer_cleaner = getchar() != '\n' && buffer_cleaner != EOF);
+
+			printf("Enter the size of the second array: ");
+			if(scanf("%d", m) != 1 || *m <= 0)
 				{
-					if(array1[i] == array2[j])
-						{
-							flag_duplicate = 0;
-
-							for(int k = i + 1; k < n; k++)
-								{
-									if(array1[k] == array1[i])
-									{
-										flag_duplicate++;
-										break;
-									}
-								}
-
-							if(!flag_duplicate)
-								Amount_of_intersections++;
-							
-							break;
-						}
-			}
+					while(buffer_cleaner = getchar() != '\n' && buffer_cleaner != EOF);
+					puts("Invalid input!");
+					exit(1);
+				}
+			while(buffer_cleaner = getchar() != '\n' && buffer_cleaner != EOF);
 		}
 
-		int result[Amount_of_intersections];
-
-		for(int i = 0; i < n; i++)
+	void FillArraysUp(int array1[], int array2[], int n, int m)
 		{
-			for(int j = 0; j < n; j++)
-			{
-				if(array1[i] == array2[j])
-					{
-						flag_duplicate = 0;
+			for(int i = 0; i < n; i++)
+				{
+					array1[i] = rand() % 10;
+					printf("%d ", array1[i]);
+				}
+			puts("");
 
-						for(int k = i + 1; k < n; k++)
-							{
-								if(array1[k] == array1[i])
+			for(int i = 0; i < m; i++)
+				{
+					array2[i] = rand() % 10;
+					printf("%d ", array2[i]);
+				}
+			puts("");
+		}
+
+	void Intersection(int array1[], int array2[], int n, int m)
+		{
+			int Amount_of_intersections = 0;
+			int flag_duplicate;
+			int result_index = 0;
+
+			for(int i = 0; i < n; i++)
+				{
+					flag_duplicate = 0;
+
+					for(int k = i + 1; k < n && i < n - 1; k++)
+						{
+							if(array1[k] == array1[i])
 								{
-									flag_duplicate++;
+									flag_duplicate = 1;
 									break;
 								}
-							}
+						}
 
-						if(!flag_duplicate)
-							{
-								result[i] = array1[i];
-								printf("%d ", result[i]);
-							}
-						
-						break;
-					}
-			}
+					if(flag_duplicate)
+						continue;
+
+					for(int j = 0; j < m; j++)
+						{
+							if(array1[i] == array2[j])
+								{
+									Amount_of_intersections++;
+									break;
+								}
+						}
+				}
+
+			int result[Amount_of_intersections];
+
+			for(int i = 0; i < n; i++)
+				{
+					flag_duplicate = 0;
+
+					for(int k = i + 1; k < n && i < n - 1; k++)
+						{
+							if(array1[k] == array1[i])
+								{
+									flag_duplicate = 1;
+									break;
+								}
+						}
+
+					if(flag_duplicate)
+						continue;
+
+					for(int j = 0; j < m; j++)
+						{
+							if(array1[i] == array2[j])
+								{
+									result[result_index] = array1[i];
+									printf("%d ", result[result_index]);
+									result_index++;
+									break;
+								}
+						}
+				}
 		}
-			
-	}
 		
 	void Union(int array1[], int array2[], int n)
-	{
-		int Amount_of_unions = 0;
-
-		for(int i = 0; i < n; i++)
 		{
-			for(int j = 0; j < n; j++)
-			{
-				if(array1[i] != array2[j])
+			int Amount_of_unions = 0;
+
+			for(int i = 0; i < n; i++)
 				{
-					Amount_of_unions++;
-				}
-			}
-		}
-
-		int result[Amount_of_unions];
-		int k = 0;
-
-		for(int i = 0; i < n; i++)
-		{
-			// if(k == Amount_of_unions)
-			// 	break;
-
-			for(int j = 0; j < n; j++)
-				{
-					if(array1[i] != array2[j])
+					for(int j = 0; j < n; j++)
 					{
-						result[k] = array1[i];
-						k++;
+						if(array1[i] != array2[j])
+							Amount_of_unions++;
 					}
 				}
-		}
 
-		for(int i = 0; i < Amount_of_unions; i++)
-		{
-			printf("%d ", result[i]);
+			int result[Amount_of_unions];
+			int k = 0;
+
+			for(int i = 0; i < n; i++)
+				{
+					for(int j = 0; j < n; j++)
+						{
+							if(array1[i] != array2[j])
+							{
+								result[k] = array1[i];
+								k++;
+							}
+						}
+				}
+
+			for(int i = 0; i < Amount_of_unions; i++)
+			{
+				printf("%d ", result[i]);
+			}
 		}
-	}
